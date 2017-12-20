051a61a398ce768e148c29fb53ef5c83da7ff89a
Ivan <ivan.grubisic.mail@gmail.com>
2017-12-20 16:04:34 +0100

    A commit and push for Marko

--- TestScheduling/src/hr/unizg/fer/hmo/ts/demo/Main.java
+++ TestScheduling/src/hr/unizg/fer/hmo/ts/demo/Main.java
@@ -3,7 +3,7 @@ package hr.unizg.fer.hmo.ts.demo;
 public class Main {
 
 	public static void main(String[] args) {
-		String hi = "Hello world!";
+		String hi = "RESEARCH FLAT MOON!";
 		System.out.println("Entry point of our project");
 		System.out.println("says");
 		System.out.println(hi);
