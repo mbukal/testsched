04efc72c93d5b654a2cde9fb796b56a2c24a0f07
Ivan <ivan.grubisic.mail@gmail.com>
2017-12-20 16:47:26 +0100

    Fixed Main.java

--- TestScheduling/src/hr/unizg/fer/hmo/ts/demo/Main.java
+++ TestScheduling/src/hr/unizg/fer/hmo/ts/demo/Main.java
@@ -3,12 +3,9 @@ package hr.unizg.fer.hmo.ts.demo;
 public class Main {
 
 	public static void main(String[] args) {
-		String hi = "RESEARCH FLAT MOON!";
+		String hi = "Hello world!";
 		System.out.println("Entry point of our project");
 		System.out.println("says");
 		System.out.println(hi);
-		
-		System.out.println("Dummy line");
-
 	} 
 }
