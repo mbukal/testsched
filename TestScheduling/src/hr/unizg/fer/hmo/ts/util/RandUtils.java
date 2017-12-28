addbe5aaba255d9ab14f33c8a136eba60ff86a86
Marko Bukal <marko.bukal@fer.hr>
2017-12-28 13:53:51 +0100

    All evolutionary operators implemented and GA put to use through demo class.

--- TestScheduling/src/hr/unizg/fer/hmo/ts/util/RandUtils.java
+++ TestScheduling/src/hr/unizg/fer/hmo/ts/util/RandUtils.java
@@ -5,6 +5,8 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public final class RandUtils {
 	private static final Random rand = ThreadLocalRandom.current();
+	public final static int HEADS = 0;
+	public final static int TAILS = 1;
 
 	public static int randomElement(int[] arr) {
 		return arr[rand.nextInt(arr.length)];
@@ -14,5 +16,13 @@ public final class RandUtils {
 	public static void shuffle(int[] arr) {
 		for (int i = arr.length - 1; i > 0; i--)
 			ArrayUtils.swap(arr, i, rand.nextInt(i + 1));
+	}
+	
+	public static int flipCoin() {
+		if (rand.nextDouble() < 0.5) {
+			return HEADS;
+		} else {
+			return TAILS;
+		}
 	}
 }
